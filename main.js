0ab6e308ed533197ec32e3643c26ee4193454f10
zerobug110 <mahabojang110@gmail.com>
2020-11-26 03:48:12 +0000

    complet the modal  popup

--- main.js
+++ main.js
@@ -1,30 +1,45 @@
-// const openModalButtons = document.querySelectorAll('[data-modal-target]');
-// const closeModalButtons = document.querySelectorAll('[data-close-button');
-// const overlay = document.getElementById('overlay');
+//variables
 
-// openModalButtons.forEach( button => {
-//     button.addEventListener('click',() => {
-//         const modal = document.querySelector(button.dataset.modalTarget);
-//         openModal(modal);
-//     });
-// });
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button');
+const overlay = document.getElementById('overlay');
 
-// closeModalButtons.forEach( button => {
-//     button.addEventListener('click',() => {
-//         const modal = button.closes('.modal');
-//         closeModal(modal);
-//     });
-// });
+//open modal when click on project
+openModalButtons.forEach( button => {
+    button.addEventListener('click',() => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    });
+});
+
+//close modal when click outside of the modal
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active');
+    modals.forEach(modal => {
+        closeModal(modal)
+    });
+});
+
+//close modal when click on project
+closeModalButtons.forEach( button => {
+    button.addEventListener('click',() =>  {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    });
+});
 
 
-// function openModal(modal) {
-//     if (modal == null) return modal.classList.add('active');
-//     modal.classList.add('active');
-//     overlay.classList.add('active');
-// }
+//add active class when on project modal
+function openModal(modal) {
+    if (modal == null) return 
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
 
-// function closeModal(modal) {
-//     if (modal == null) return 
-//     modal.classList.remove('active');
-//     overlay.classList.remove('active');
-// }
+
+//remove class when click close
+function closeModal(modal) {
+    if (modal == null) return 
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
