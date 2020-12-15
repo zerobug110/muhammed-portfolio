2b9917b361dd7323b459b950939a8716aff7fe54
zerobug110 <mahabojang110@gmail.com>
2020-12-15 03:09:05 +0000

    make the website reponsive

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+//HAMBURGER NAV-BAR AND AUTO DATE GENERATE
+
+//get the  nav items
+const burger = document.querySelector('.lines');
+const navLinks = document.querySelector('.nav-bar__items')
+const open = document.querySelector('.nav-bar__show');
+const links = document.querySelectorAll('.nav-bar__links')
+const date = document.getElementById('date');
+
+// toggle when click
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-bar__show');
+});
+
+//generate the current date
+date.innerHTML = new Date().getFullYear();
+
+
+//////////////////////////modal///////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 //variables
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
 const closeModalButtons = document.querySelectorAll('[data-close-button');
